8ce72131a93a01385560614266ca0aa8acd7fbca
Fabian Meyer <user8324@posteo.net>
2019-03-03 20:39:25 +0100

    Add smoothing filter example

---
+++ examples/filter_smooth.cpp
@@ -0,0 +1,32 @@
+/* filter_smooth.cpp
+ *
+ * Author: Fabian Meyer
+ * Created On: 02 Mar 2019
+ */
+
+#include <iostream>
+#include <cve/filter/gauss_filter.h>
+#include <cve/imageio/pgm.h>
+
+using namespace cve;
+
+int main(int argc, const char **argv)
+{
+    if(argc != 2)
+    {
+        std::cerr << "usage: filter_smooth <filename>" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Load \"" << argv[1] << "\"" << std::endl;
+    ImageGray img;
+    cve::pgm::load(argv[1], img);
+
+    std::cout << "Apply gauss filter" << std::endl;
+    GaussFilter<float> gaussFilter(4);
+
+    ImageGray oimg;
+    gaussFilter.apply<ImageGray>(img, oimg);
+
+    return 0;
+}
