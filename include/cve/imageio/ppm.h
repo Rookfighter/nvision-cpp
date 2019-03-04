cf03497545311cb23fe63e0e06b056cf0bccb449
Fabian Meyer <user8324@posteo.net>
2019-03-04 14:51:38 +0100

    Implement reading ppm RGB files

--- include/cve/imageio/ppm.h
+++ include/cve/imageio/ppm.h
@@ -1,10 +1,10 @@
-/* pgm.h
+/* ppm.h
  *
  * Author: Fabian Meyer
- * Created On: 03 Mar 2019
+ * Created On: 04 Mar 2019
  */
-#ifndef CVE_IMAGEIO_PGM_H_
-#define CVE_IMAGEIO_PGM_H_
+#ifndef CVE_IMAGEIO_PPM_H_
+#define CVE_IMAGEIO_PPM_H_
 
 #include <string>
 #include <fstream>
@@ -12,20 +12,20 @@
 
 namespace cve
 {
-namespace pgm
+namespace ppm
 {
-    void load(const std::string &filename, ImageGray &img)
+    void load(const std::string &filename, ImageRGB &img)
     {
         std::ifstream is(filename);
 
         if(is.bad() || is.fail())
             throw std::runtime_error("cannot open " + filename);
 
-        // find start signature "P5"
+        // find start signature "P6"
         while(!is.eof() && is.get() != 'P');
 
-        if(is.eof() || is.get() != '5')
-            throw std::runtime_error(filename + ": missing PGM start sequence");
+        if(is.eof() || is.get() != '6')
+            throw std::runtime_error(filename + ": missing PPM start sequence");
 
         int value = is.get();
         while(!is.eof() && value != ' ' && value != '\n')
@@ -40,7 +40,6 @@ namespace pgm
             // update value to first char of line
             value = is.get();
         }
-
 
         // trim all empty lines
         while(!is.eof() && value == '\n')
@@ -71,41 +70,44 @@ namespace pgm
         while(!is.eof() && (value < 48 || value >= 58))
             value = is.get();
 
+        while(!is.eof() && (value >= 48 || value < 58))
+            value = is.get();
+
         // find next line
         while(is.eof() && value != '\n')
             value = is.get();
 
         if(is.eof())
-            throw std::runtime_error(filename + ": incomplete PGM file");
+            throw std::runtime_error(filename + ": incomplete PPM file");
 
         if(width <= 0 || height <= 0)
-            throw std::runtime_error(filename + ": invalid PGM image size");
+            throw std::runtime_error(filename + ": invalid PPM image size");
         img.resize(height, width);
 
         // read image data, data is stored in row major format
-        for(ImageGray::Index row = 0; row < height; ++row)
+        for(ImageRGB::Index row = 0; row < height; ++row)
         {
-            for(ImageGray::Index col = 0; col < width; ++col)
+            for(ImageRGB::Index col = 0; col < width; ++col)
             {
-                img(row, col)(0) = is.get();
+                img(row, col) << is.get(), is.get(), is.get();
             }
         }
     }
 
-    void save(const std::string &filename, const ImageGray &img)
+    void save(const std::string &filename, const ImageRGB &img)
     {
         std::ofstream os(filename);
 
-        os << "P5\n"
+        os << "P6\n"
             << img.cols() << ' '
             << img.rows() << '\n'
             << "255" << '\n';
 
-        for(ImageGray::Index row = 0; row < img.rows(); ++row)
+        for(ImageRGB::Index row = 0; row < img.rows(); ++row)
         {
-            for(ImageGray::Index col = 0; col < img.cols(); ++col)
+            for(ImageRGB::Index col = 0; col < img.cols(); ++col)
             {
-                os << img(row, col)(0);
+                os << img(row, col)(0), img(row, col)(1), img(row, col)(2);
             }
         }
     }
