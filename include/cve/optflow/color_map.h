6b9da8210af395a801f70b0ed06bbaa519b9cb2b
Fabian Meyer <user8324@posteo.net>
2019-05-18 13:08:05 +0200

    Use HSV coloring for optical flow

--- include/cve/optflow/color_map.h
+++ include/cve/optflow/color_map.h
@@ -8,6 +8,7 @@
 #define CVE_OPTFLOW_COLOR_MAP_H_
 
 #include <Eigen/Geometry>
+#include "cve/color_converter.h"
 #include "cve/math.h"
 
 namespace cve
@@ -28,6 +29,14 @@ namespace cve
             {
                 static_assert(Image::Depth >= 3, "Image needs at least depth of 3");
 
+                Eigen::Array<Scalar, Eigen::Dynamic, Eigen::Dynamic> intensity;
+                intensity.resize(flowImg.rows(), flowImg.cols());
+                for(Index c = 0; c < flowImg.cols(); ++c)
+                    for(Index r = 0; r < flowImg.rows(); ++r)
+                        intensity(r, c) = flowImg(r, c).matrix().norm();
+                intensity -= intensity.minCoeff();
+                intensity /= intensity.maxCoeff();
+
                 img.resize(flowImg.rows(), flowImg.cols());
                 for(Index c = 0; c < flowImg.cols(); ++c)
                 {
@@ -35,63 +44,18 @@ namespace cve
                     {
                         Eigen::Matrix<Scalar, 2, 1> vec = flowImg(r, c).matrix();
 
-                        Scalar phi = std::atan2(vec(1), vec(0)) + pi<Scalar>();
+                        Scalar phi = std::atan2(vec(1), vec(0));
+                        if(phi < 0)
+                            phi += 2 * pi<Scalar>();
 
-                        Scalar intense = vec.norm();
-                        if(intense > 1)
-                            intense = 0;
+                        Scalar intense = intensity(r, c);
+                        Eigen::Array<Scalar, 3, 1> hsv;
+                        hsv(0) = phi * 180 / pi<Scalar>();
+                        hsv(1) = intense;
+                        hsv(2) = 0.9;
 
-                        Scalar alpha;
-                        Scalar beta;
-                        Scalar piquart = pi<Scalar>() / 4;
-                        if(phi < piquart)
-                        {
-                            beta  = phi / piquart;
-                            alpha = 1.0 - beta;
-                            img(r, c, 0) = intense * (alpha * 255 + beta * 255);
-                            img(r, c, 1) = 0;
-                            img(r, c, 2) = intense * beta * 255;
-                        }
-                        else if(phi < 2 * piquart)
-                        {
-                            beta  = (phi - piquart) / piquart;
-                            alpha = 1.0 - beta;
-                            img(r, c, 0) = intense * (alpha * 255 + beta *  64);
-                            img(r, c, 1) = intense * (beta * 64);
-                            img(r, c, 2) = intense * (alpha * 255 + beta * 255);
-                        }
-                        else if(phi < 3 * piquart)
-                        {
-                            beta  = (phi - 2 * piquart) / piquart;
-                            alpha = 1.0 - beta;
-                            img(r, c, 0) = intense * (alpha * 64);
-                            img(r, c, 1) = intense * (alpha * 64 + beta * 255);
-                            img(r, c, 2) = intense * (alpha * 255 + beta * 255);
-                        }
-                        else if(phi < 4 * piquart)
-                        {
-                            beta  = (phi - 3 * piquart) / piquart;
-                            alpha = 1.0 - beta;
-                            img(r, c, 0) = 0;
-                            img(r, c, 1) = intense * (alpha * 255 + beta * 255);
-                            img(r, c, 2) = intense * (alpha * 255);
-                        }
-                        else if(phi < 6 * piquart)
-                        {
-                            beta  = (phi - 4 * piquart) / (2 * piquart);
-                            alpha = 1.0 - beta;
-                            img(r, c, 0) = intense * (beta * 255);
-                            img(r, c, 1) = intense * (alpha * 255 + beta * 255);
-                            img(r, c, 2) = 0;
-                        }
-                        else if (phi <= 8 * piquart)
-                        {
-                            beta  = (phi - 6 * piquart) / (2 * piquart);
-                            alpha = 1.0 - beta;
-                            img(r, c, 0) = intense * (alpha * 255 + beta * 255);
-                            img(r, c, 1) = intense * (alpha * 255);
-                            img(r, c, 2) = 0;
-                        }
+                        ColorConverter conv;
+                        img(r, c) = conv.hsv2rgb<Scalar, typename Image::Scalar>(hsv);
                     }
                 }
             }
