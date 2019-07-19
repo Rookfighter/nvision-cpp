1f80229d51e08fc203144be853267ddc220daa2a
Fabian Meyer <user8324@posteo.net>
2019-07-19 09:25:59 +0530

    Remove png and jpg loading

--- include/cve/imageio/imageio.h
+++ include/cve/imageio/imageio.h
@@ -9,8 +9,8 @@
 
 #include "cve/imageio/ppm.h"
 #include "cve/imageio/pgm.h"
-#include "cve/imageio/png.h"
-#include "cve/imageio/jpg.h"
+// #include "cve/imageio/png.h"
+// #include "cve/imageio/jpg.h"
 
 namespace cve
 {
@@ -32,10 +32,10 @@ namespace cve
             pgm::load(filename, img);
         else if(ext == "ppm")
             ppm::load(filename, img);
-        else if(ext == "png")
-            png::load(filename, img);
-        else if(ext == "jpg" || ext == "jpeg")
-            jpg::load(filename, img);
+        // else if(ext == "png")
+        //     png::load(filename, img);
+        // else if(ext == "jpg" || ext == "jpeg")
+        //     jpg::load(filename, img);
         else
             throw std::runtime_error("imload: unknown file extension \"" + ext + "\"");
     }
