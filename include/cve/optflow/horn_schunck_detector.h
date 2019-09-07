9e5054970cde188d14fad442b8342696f89ba58f
Fabian Meyer <user8324@posteo.net>
2019-09-07 11:28:09 +0530

    Add tidy up horn schunk detector and add docs

--- include/cve/optflow/horn_schunck_detector.h
+++ include/cve/optflow/horn_schunck_detector.h
@@ -7,40 +7,54 @@
 #ifndef CVE_HORN_SCHUNCK_DETECTOR_H_
 #define CVE_HORN_SCHUNCK_DETECTOR_H_
 
-#include "cve/filter/gauss_filter.h"
+#include "cve/filter/box_filter.h"
 #include "cve/filter/central_differences_filter.h"
 
 namespace cve
 {
+    /** Optical flow detector which uses the Horn-Schunk method.
+      * It basically solves the global energy functional
+      *
+      * Integral( (Ix * u + Iy * v + It)^2 + alpha^2 (ux^2 + uy^2 + vx^2 + vy^2) )
+      *
+      * Using Euler-Lagrange equations:
+      *
+      * Ix * (Ix * u + Iy * v + It) - alpha^2 lap(u) = 0
+      * Iy * (Ix * u + Iy * v + It) - alpha^2 lap(v) = 0
+      */
     template<typename Scalar,
-        typename SmoothFilter = GaussFilter<Scalar>,
+        typename SmoothFilter = BoxFilter<Scalar>,
         typename GradientFilter = CentralDifferencesFilter<Scalar>>
     class HornSchunckDetector
     {
     private:
-        Index maxIt_;
+        Index iterations_;
         Scalar alpha_;
         SmoothFilter smoothFilter_;
         GradientFilter gradientFilter_;
     public:
         HornSchunckDetector()
-            : HornSchunckDetector(10, 2)
+            : HornSchunckDetector(100, 15)
         { }
 
         HornSchunckDetector(const Index iterations, const Scalar alpha)
-            : maxIt_(iterations), alpha_(alpha),
+            : iterations_(iterations), alpha_(alpha),
             smoothFilter_(), gradientFilter_()
-        {
-        }
+        { }
 
-        void setAlpha(const Scalar alpha)
+        /** Sets the regularization constant for the Horn-Schunck method.
+          * Higher values make the result smoother.
+          * @param alpha regularization constant */
+        void setRegularizationConstant(const Scalar alpha)
         {
             alpha_ = alpha;
         }
 
-        void setMaxIterations(const Index iterations)
+        /** Set the number of iterations to the solve the equation system.
+          * @param iterations number of iterations for the solver */
+        void setIterations(const Index iterations)
         {
-            maxIt_ = iterations;
+            iterations_ = iterations;
         }
 
         void setSmoothFilter(const SmoothFilter &filter)
@@ -54,7 +68,7 @@ namespace cve
         }
 
         template<typename ScalarA>
-        void apply(const Eigen::Tensor<ScalarA, 3> &imgA,
+        void operator()(const Eigen::Tensor<ScalarA, 3> &imgA,
             const Eigen::Tensor<ScalarA, 3> &imgB,
             Eigen::Tensor<Scalar, 3> &flowImg) const
         {
@@ -75,22 +89,19 @@ namespace cve
 
             Eigen::Tensor<Scalar, 3> flowU(height, width, 1);
             Eigen::Tensor<Scalar, 3> flowV(height, width, 1);
-            Eigen::Tensor<Scalar, 3> step(height, width, 1);
+            Eigen::Tensor<Scalar, 3> dataTerm;
+
+            Eigen::Tensor<Scalar, 3> stepSizes = gradX.constant(1) / (gradX * gradX + gradY * gradY + 4 * alpha_ * alpha_);
 
             flowU.setZero();
             flowV.setZero();
-
-            Scalar lambda = 4 * alpha_ * alpha_;
-
-            Eigen::Tensor<Scalar, 3> stepSizes = 1 / (gradX * gradX + gradY * gradY + lambda);
-
-            for(Index i = 0; i < maxIt_; ++i)
+            for(Index i = 0; i < iterations_; ++i)
             {
-                step = gradX * flowU + gradY * flowV + gradT;
-                flowU -= stepSizes * gradX * step;
-                flowV -= stepSizes * gradY * step;
                 smoothFilter_(flowU);
                 smoothFilter_(flowV);
+                dataTerm = gradX * flowU + gradY * flowV + gradT;
+                flowU -= stepSizes * dataTerm * gradX;
+                flowV -= stepSizes * dataTerm * gradY;
             }
 
             flowImg.resize(height, width, 2);
